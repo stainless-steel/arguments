983a5b7969de4b2d42084fa538fe1f33acff48c0
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-11-12 11:34:58 +0100

    Update options and switch to std::io::Result

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,3 +1,4 @@
+use std::io::Result;
 use std::str::FromStr;
 
 use options::Options;
@@ -11,6 +12,67 @@ pub struct Arguments {
     pub options: Options,
     /// Unclassified arguments.
     pub orphans: Vec<String>,
+}
+
+impl Arguments {
+    /// Parse command-line arguments.
+    pub fn parse<I: Iterator<Item = String>>(mut stream: I) -> Result<Self> {
+        let mut arguments = Self {
+            program: match stream.next() {
+                Some(ref program) if !program.starts_with("--") => String::from(&program[..]),
+                _ => raise!("expected a name as the first argument"),
+            },
+            options: Default::default(),
+            orphans: Default::default(),
+        };
+
+        let mut previous: Option<String> = None;
+
+        macro_rules! set(
+            ($name:expr) => (
+                if let Some(name) = $name.strip_prefix("no-") {
+                    if name.is_empty() {
+                        raise!("expected a name right after “--no-”");
+                    }
+                    set!(name, "false");
+                } else {
+                    set!($name, "true");
+                }
+            );
+            ($name:expr, $value:expr) => (
+                match arguments.options.get_mut::<Vec<_>>($name) {
+                    Some(array) => {
+                        array.push(String::from($value));
+                    }
+                    _ => {
+                        arguments.options.set($name, vec![String::from($value)]);
+                    }
+                }
+            );
+        );
+
+        for chunk in stream {
+            if let Some(name) = chunk.strip_prefix("--") {
+                if let Some(ref name) = previous {
+                    set!(name);
+                }
+                if name.is_empty() {
+                    raise!("expected a name right after “--”");
+                }
+                previous = Some(String::from(name));
+            } else if let Some(ref name) = previous {
+                set!(name, chunk);
+                previous = None;
+            } else {
+                arguments.orphans.push(chunk);
+            }
+        }
+        if let Some(ref name) = previous {
+            set!(name);
+        }
+
+        Ok(arguments)
+    }
 }
 
 impl Arguments {
@@ -33,5 +95,51 @@ impl Arguments {
                     .map(|string| string.parse().ok())
                     .collect::<Option<Vec<_>>>()
             })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Arguments;
+
+    macro_rules! strings(($slices:expr) => ($slices.iter().map(|s| s.to_string())));
+
+    #[test]
+    fn program() {
+        let arguments = vec!["--a", "--b"];
+        match Arguments::parse(strings!(arguments)) {
+            Ok(_) => unreachable!(),
+            Err(_) => {}
+        }
+    }
+
+    #[test]
+    fn arrays() {
+        let arguments = vec!["a", "--b", "1", "--b", "2"];
+        let arguments = Arguments::parse(strings!(arguments)).unwrap();
+        assert_eq!(arguments.get_all::<usize>("b").unwrap(), &[1, 2]);
+    }
+
+    #[test]
+    fn booleans() {
+        let arguments = vec!["a", "--b", "--no-c", "--d"];
+        let arguments = Arguments::parse(strings!(arguments)).unwrap();
+        assert_eq!(arguments.get::<bool>("b").unwrap(), true);
+        assert_eq!(arguments.get::<bool>("c").unwrap(), false);
+        assert_eq!(arguments.get::<bool>("d").unwrap(), true);
+    }
+
+    #[test]
+    fn orphans() {
+        let arguments = vec!["a", "b", "--c", "d", "e", "--f"];
+        let Arguments { orphans, .. } = Arguments::parse(strings!(arguments)).unwrap();
+        assert_eq!(&orphans, &["b", "e"]);
+    }
+
+    #[test]
+    fn overrides() {
+        let arguments = vec!["a", "--b", "1", "--b", "2"];
+        let arguments = Arguments::parse(strings!(arguments)).unwrap();
+        assert_eq!(arguments.get::<usize>("b").unwrap(), 2);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //! #     "--baz", "69",
 //! #     "--qux", "Hello, world!",
 //! # ];
-//! # let arguments = arguments.iter().map(|a| a.to_string());
+//! # let arguments = arguments.iter().map(|argument| argument.to_string());
 //! let arguments = arguments::parse(arguments).unwrap();
 //!
 //! assert_eq!(arguments.program, "foo");
@@ -22,44 +22,20 @@
 //! assert_eq!(arguments.get::<String>("qux").unwrap(), "Hello, world!");
 //! ```
 
-macro_rules! raise(($message:expr) => (return Err(crate::Error($message))));
+macro_rules! raise(
+    ($message:expr) => (
+        return Err(std::io::Error::other($message))
+    );
+);
 
 mod arguments;
-mod parser;
 
-/// An error.
-pub struct Error(pub &'static str);
-
-/// A result.
-pub type Result<T> = std::result::Result<T, Error>;
+pub use options::Options;
 
 pub use crate::arguments::Arguments;
-pub use options::Options;
-pub use parser::Parser;
-
-impl std::error::Error for Error {
-    #[inline]
-    fn description(&self) -> &str {
-        self.0
-    }
-}
-
-impl std::fmt::Debug for Error {
-    #[inline]
-    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        self.0.fmt(formatter)
-    }
-}
-
-impl std::fmt::Display for Error {
-    #[inline]
-    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        self.0.fmt(formatter)
-    }
-}
 
 /// Parse command-line arguments.
 #[inline]
-pub fn parse<I: Iterator<Item = String>>(stream: I) -> Result<Arguments> {
-    Parser::new().parse(stream)
+pub fn parse<I: Iterator<Item = String>>(stream: I) -> std::io::Result<Arguments> {
+    Arguments::parse(stream)
 }
